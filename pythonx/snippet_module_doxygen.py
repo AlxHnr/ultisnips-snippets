084eb26ed9d0b10d512a746aaf6155fb887472ea
Alexander Heinrich <alxhnr@users.noreply.github.com>
2020-03-15 17:01:16 +0100

    Move doxygen functions into own python file

---
+++ pythonx/snippet_module_doxygen.py
@@ -0,0 +1,36 @@
+from snippet_module_c import *
+from snippet_module_base import *
+import vim
+
+def uses_single_line_c_comments():
+  return vim.eval("&commentstring").startswith("//")
+
+def get_doc_empty_line(pre_indented):
+  if uses_single_line_c_comments():
+    return '\n' + pre_indented + '///'
+  elif format_options_have_comment_leader():
+    return '\n' + pre_indented + ' *'
+  else:
+    return '\n'
+
+def get_doc_commend_ending():
+  if uses_single_line_c_comments():
+    return ''
+  else:
+    return "*/"
+
+def get_doc_last_line(pre_indented):
+  if uses_single_line_c_comments():
+    return ''
+  elif format_options_have_comment_leader():
+    return "\n" + pre_indented + " " + get_doc_commend_ending()
+  else:
+    return "\n" + pre_indented + get_doc_commend_ending()
+
+def get_doc_prefix():
+  if uses_single_line_c_comments():
+    return '/// '
+  elif format_options_have_comment_leader():
+    return ' * '
+  else:
+    return '  '
